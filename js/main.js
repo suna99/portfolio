404bc4a0170a6575394df33a52f917589e907c3f
sunachoiAAA <102567671+sunachoiAAA@users.noreply.github.com>
2022-08-18 10:33:15 +0900

    portfolio
    portfolio

---
+++ js/main.js
@@ -0,0 +1,89 @@
+gsap.registerPlugin(ScrollTrigger);
+//gsap.defaults({ ease: "none", duration: 2 });
+
+const parallaxCont = document.getElementById(".parallax_cont");
+const parallaxItem = document.querySelector(".content_item");
+const rowPage1 = document.querySelector(".row_page1");
+const rowPage2 = document.querySelector(".row_page2");
+const rowPageWrap1 = document.querySelector(".row_page_wrap1");
+const rowPageWrap2 = document.querySelector(".row_page_wrap2");
+
+// window.addEventListener("scroll", () => {
+//   let scrollTop = window.pageYOffset || document.documentElement.scrollTop || window.scrollY;
+//   let tl = new TimelineLite();
+
+//   if (scrollTop > document.getElementById("#section2").offsetTop) {
+//     tl.set( document.querySelector(".nav"), { className: '+=show' } , "start")
+//   } else {
+//     tl.to(document.querySelector(".nav"), 1, { display: 'none' });
+//   }
+
+// });
+
+
+//nav
+
+gsap.set(".nav", { y: -100 });
+const showAnim = gsap.from('.nav', {
+  yPercent: 100,
+  paused: true,
+  duration: 0.2
+}).progress(1);
+
+ScrollTrigger.create({
+  trigger: "#section5",
+  start: "top bottom",
+  end: 8000,
+  markers: true,
+  onUpdate: (self) => {
+    self.direction === -1 ? showAnim.play() : showAnim.reverse()
+  }
+});
+
+
+function horizontalScrolling(wrap, page) {
+  gsap.to(wrap, {
+    xPercent: -100,
+    x: () => innerWidth,
+    ease: "none",
+    invalidateOnRefresh: true,
+    scrollTrigger: {
+      trigger: page,
+      start: "top top",
+      markers: true,
+      scrub: 1,
+      pin: page,
+      end: "+=" + (innerWidth * 3)
+    },
+  });
+}
+
+horizontalScrolling(".row_page_wrap1", ".row_page1");
+horizontalScrolling(".row_page_wrap2", ".row_page2");
+horizontalScrolling(".row_page_wrap3", ".row_page3");
+horizontalScrolling(".row_page_wrap4", ".row_page4");
+
+
+ScrollTrigger.create({
+  trigger: '#section3',
+  pin: true,
+  start: 'top top',
+  scrub: 0.5,
+  endTrigger: "#section4",
+  end: "top bottom",
+  pinSpacing: false,
+  //markers: true
+});
+
+ScrollTrigger.create({
+  trigger: '#section5',
+  pin: true,
+  start: 'top top',
+  scrub: 0.5,
+  endTrigger: "#section7",
+  end: "top bottom",
+  pinSpacing: false,
+  //markers: true
+});
+
+
