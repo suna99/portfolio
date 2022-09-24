db1cbcebb85a7d4cb4fe779b1eb1e7a14f1d57f6
SunaChoi <102567671+suna99@users.noreply.github.com>
2022-09-24 15:55:56 +0900

    2022.09.24

--- js/main.js
+++ js/main.js
@@ -2,12 +2,66 @@ let s = skrollr.init({
   smoothScrolling: true,
 });
 
-document.querySelectorAll(".portfolio_list ul li a").forEach((li) => {
-  li.addEventListener("click", (e) => {
-    e.preventDefault();
-    document.querySelector(li.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
+// document.querySelectorAll(".portfolio_list ul li a").forEach((li) => {
+//   li.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     document.querySelector(li.getAttribute("href")).scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   });
+// });
+
+document.querySelector("a.aboutMe").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 12000,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+document.querySelector("a.skills").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 19000,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+document.querySelector("a.javascript").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 26000,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+document.querySelector("a.siteWorks").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 33000,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+document.querySelector("a.frontEnd").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 37500,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+document.querySelector("a.footer").addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 41000,
+    left: 0,
+    behavior: "smooth",
   });
 });
 
